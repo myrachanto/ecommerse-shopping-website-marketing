60e826caf4c1e82f34ec2691ca797c24e960aaff
Anthony Miracho <myrachanto@ggmail.com>
2021-03-12 17:41:53 +0300

    more changes

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -26,7 +26,7 @@
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg></div>
         <div class="relative">
-        <div class="absolute top-0 right-0 text-sm z-10 bg-red-500 px-1 rounded-xl text-white m-2">
+        <div class="absolute top-0 right-0 text-sm z-10 bg-red-500 px-1 rounded-xl text-white ">
           <span>5</span>
         </div>
         <div class="m-2">
